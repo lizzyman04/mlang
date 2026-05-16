9902c3a7920ca01022f2fce974dab768f02764c2
lizzyman04 <arlindoabdullm@gmail.com>
2026-05-16 07:40:57 +0200

    feat(lexer): add try, catch, Err, Ok keywords

--- src/core/lexer/rules.rs
+++ src/core/lexer/rules.rs
@@ -5,7 +5,7 @@ pub fn is_keyword(word: &str) -> bool {
     let keywords: HashSet<&str> = [
         "int", "dec", "txt", "bool", "void", "true", "false", "main", "print", "return",
         "if", "else", "array", "while", "for", "in", "break", "continue", "struct",
-        "module", "import",
+        "module", "import", "try", "catch", "Err", "Ok",
     ]
     .iter()
     .copied()
